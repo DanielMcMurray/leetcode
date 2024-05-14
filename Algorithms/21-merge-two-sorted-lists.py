24bbd20d7123e5b81f1c9346f097cbb7aeb2f35e
Danny McMurray <danny.mcmurrayjnr@gmail.com>
2024-05-15 00:28:52 +0100

    started 21

---
+++ Algorithms/21-merge-two-sorted-lists.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        lst = []
+        for elem 
+
+    mergeTwoLists(list1 = [1,2,4], list2 = [1,3,4])
