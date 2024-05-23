2ae02bc633b27bbfa7aa16345a16e394be36f01c
Daniel McMurray CS2020 <daniel.mcmurray.2020@uni.strath.ac.uk>
2024-05-23 21:01:58 +0100

    comleted 21

--- Algorithms/21-merge-two-sorted-lists.py
+++ Algorithms/21-merge-two-sorted-lists.py
@@ -10,7 +10,24 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        lst = []
-        for elem 
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        
+        temp = current = ListNode(0)
+        temp.next = list1
+        while list1 and list2:
+            if list1.val < list2.val:
+                list1 = list1.next
+            else:
+                next = current.next
+                current.next = list2
+                t = list2.next
+                list2.next = next
+                list2 = t
+            current = current.next
+        current.next = list1 or list2
+        return temp.next
 
     mergeTwoLists(list1 = [1,2,4], list2 = [1,3,4])
